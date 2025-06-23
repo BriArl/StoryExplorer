2f8b2ff5a87967b173cf78e14463e4d0b719c425
BriArl <brianna.youte@gmail.com>
2025-06-22 22:21:24 -0400

    fixed directory

--- story-explorer/src/App.js
+++ story-explorer/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import stories from "./data/stories.json"
+import StoryCard from "./components/StoryCard"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Story Explorer</h1>
+            <div className="story-list">
+                {stories.map((story) => (
+                    <StoryCard key={StoryCard.id} story={story} />
+                ))}
+            </div>
+        </div>
+    );
 }
-
 export default App;
